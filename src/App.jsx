711628d8b13ea2e78d37189f9d3e3c4208061f5c
farildev <memmedovfaril@gmail.com>
2023-09-01 16:28:59 +0400

    Bugs fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import Home from './pages/Home/Home';
-import Products from './pages/Products/Products';
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
+import Home from './pages/Home';
+import Products from './pages/Products';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Basket from "./pages/Basket";
 import {Routes , Route} from "react-router-dom";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/products' element={<Products/>}/>
+        <Route path='/basket' element={<Basket/>}/>
       </Routes>
       {/* <Footer/> */}
     </>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Card from '../../components/Card/Card';
+import Card from '../components/Card';
 import { useSelector , useDispatch } from 'react-redux';
 
 function Home() {
